3d77eb72766e3f1e6d4563cdbddc77f930a7a3ec
Prince Williams <princewillzs@gmail.com>
2019-10-13 19:21:55 +0100

    Updates

---
+++ precache-manifest.54e3be43bdd7779db8683f89a144c73f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3148fa299c694341da0a63c3bb1bc10c",
+    "url": "/reactwork/index.html"
+  },
+  {
+    "revision": "c622351f71dca7013c9e",
+    "url": "/reactwork/static/css/main.f3f1e6e4.chunk.css"
+  },
+  {
+    "revision": "cda16b585b81643674cd",
+    "url": "/reactwork/static/js/2.cccf5197.chunk.js"
+  },
+  {
+    "revision": "c622351f71dca7013c9e",
+    "url": "/reactwork/static/js/main.a4da7e49.chunk.js"
+  },
+  {
+    "revision": "2c889fa6700987469265",
+    "url": "/reactwork/static/js/runtime~main.f4151859.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactwork/static/media/logo.5d5d9eef.svg"
+  }
+]);
